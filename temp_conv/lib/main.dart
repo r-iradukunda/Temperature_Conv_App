b5a4a53c429832748813c00f461f358c9a6a8458
r-iradukunda <r.iradukund@alustudent.com>
2024-09-29 23:26:05 +0200

    flutter

--- temp_conv/lib/main.dart
+++ temp_conv/lib/main.dart
@@ -1,125 +1,168 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const TempConverterApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TempConverterApp extends StatelessWidget {
+  const TempConverterApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      //debugShowCheckedModeBanner: false,
+      title: 'Temperature Converter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TemperatureConverter(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TemperatureConverter extends StatefulWidget {
+  const TemperatureConverter({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TemperatureConverterState createState() {
+    return _TemperatureConverterState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TemperatureConverterState extends State<TemperatureConverter> {
+  final TextEditingController _celsiusController = TextEditingController();
+  final TextEditingController _fahrenheitController = TextEditingController();
+  final List<String> _conversionHistory = [];
 
-  void _incrementCounter() {
+  void _convertCelsiusToFahrenheit() {
+    double? celsius = double.tryParse(_celsiusController.text);
+    if (celsius != null) {
+      double fahrenheit = (celsius * 9 / 5) + 32;
+      _fahrenheitController.text = fahrenheit.toStringAsFixed(2);
+      _addToHistory('${celsius.toStringAsFixed(2)} °C to ${fahrenheit.toStringAsFixed(2)} °F');
+    }
+  }
+
+  void _convertFahrenheitToCelsius() {
+    double? fahrenheit = double.tryParse(_fahrenheitController.text);
+    if (fahrenheit != null) {
+      double celsius = (fahrenheit - 32) * 5 / 9;
+      _celsiusController.text = celsius.toStringAsFixed(2);
+      _addToHistory('${fahrenheit.toStringAsFixed(2)} °F to ${celsius.toStringAsFixed(2)} °C');
+    }
+  }
+
+  void _addToHistory(String conversion) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _conversionHistory.insert(0, conversion);
+      if (_conversionHistory.length > 3) {
+        _conversionHistory.removeLast(); // Keep only the last 3 entries
+      }
+    });
+  }
+
+  void _resetFields() {
+    setState(() {
+      _celsiusController.clear();
+      _fahrenheitController.clear();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text(
+          'Temperature Converter',
+          style: TextStyle(
+            fontStyle: FontStyle.normal,
+            fontSize: 27,
+            color: Colors.white,
+            letterSpacing: 2.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextField(
+              controller: _celsiusController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Celsius',
+                border: OutlineInputBorder(),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 20),
+            const Text(
+                'To',
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                fontSize: 24,
+              ),
+            ),
+            TextField(
+              controller: _fahrenheitController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Fahrenheit',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: _convertCelsiusToFahrenheit,
+                  child: const Text('Convert to Fahrenheit'),
+                ),
+                ElevatedButton(
+                  onPressed: _convertFahrenheitToCelsius,
+                  child: const Text('Convert to Celsius'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _resetFields,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red, // Red background for reset button
+              ),
+              child: const Text(
+                  'Reset',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Conversion History:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _conversionHistory.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_conversionHistory[index]),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  @override
+  void dispose() {
+    _celsiusController.dispose();
+    _fahrenheitController.dispose();
+    super.dispose();
   }
 }
